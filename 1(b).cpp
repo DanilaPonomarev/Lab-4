eb1fd8e5b5509f5d594c9a9042526e45f0265fe9
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2021-12-18 11:33:30 +0300

    Create 1(b).cpp

---
+++ 1(b).cpp
@@ -0,0 +1,17 @@
+// b) удалить все символы ‘-‘ и удвоить все символы ‘&
+#include <stdio.h>
+int main()
+{
+    int a;
+    while (a != EOF && a != '.') 
+    {
+        a = getchar();
+        if (a == '-')
+            continue;
+        if (a == '&') 
+        {
+            putchar('&');
+        }
+        putchar(a);
+    }
+}
